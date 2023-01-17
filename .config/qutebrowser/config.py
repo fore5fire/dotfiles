0df493eb5f3a079170b511bdd19b582db0777504
Landon Smith <l@sturdyhippo.com>
2023-01-16 19:28:15 -0700

    qutebrowser dark settings

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,5 @@
+import dracula.draw
+
 c.scrolling.smooth = True
 
 c.editor.command = ["alacritty", "-e", "nvim", "{file}", "-c", "normal {line}G{column0}l"]
@@ -7,6 +9,7 @@ c.qt.highdpi = True
 c.search.incremental = False
 
 c.session.lazy_restore = True
+c.auto_save.session = False
 
 c.spellcheck.languages = ["en-US", "es-ES"]
 
@@ -23,4 +26,19 @@ c.url.start_pages = c.url.default_page
 
 c.window.title_format = "{perc}{current_title}"
 
+c.colors.webpage.preferred_color_scheme = "dark"
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.bg = "black"
+
 config.load_autoconfig(False)
+
+config.bind('u', 'undo --window')
+config.unbind('d')
+config.bind('dd', 'tab-close')
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
