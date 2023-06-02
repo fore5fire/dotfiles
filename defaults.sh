f7e354620d9261bf35b56ef633fdd11d8d2fa9d6
Landon Smith <l@sturdyhippo.com>
2023-06-02 13:58:37 -0600

    Add default user groups

--- defaults.sh
+++ defaults.sh
@@ -2,3 +2,5 @@
 
 xdg-mime default org.qutebrowser.qutebrowser.desktop x-scheme-handler/http
 xdg-mime default org.qutebrowser.qutebrowser.desktop x-scheme-handler/https
+
+sudo usermod -a -G pipewire -G netdev -G seat -G video -G audio l
